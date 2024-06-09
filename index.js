c721ebe03b7cf03616d157acf5665c36ddc1377c
NguyenHuyen0311 <huyenmeroria@gmail.com>
2024-06-09 17:33:32 +0700

    Tiny MCE

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require("express");
 
 // Converse lại biến trong form để truyền vào body
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Thêm các phương thức khác
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 // Thông báo
-const flash = require('express-flash');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+// TinyMCE
+const path = require('path');
 
 // ENV
 require("dotenv").config();
 
 // Kết nối database
-const database = require("./config/database")
+const database = require("./config/database");
 database.connect();
 
 // Import file System
 const systemConfig = require("./config/system");
 
-const routeAdmin = require("./routes/admin/index.route")
-const routeClient = require("./routes/client/index.route")
+const routeAdmin = require("./routes/admin/index.route");
+const routeClient = require("./routes/client/index.route");
 
 const app = express();
 const port = process.env.PORT;
@@ -32,9 +35,15 @@ app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 // Flash
-app.use(cookieParser('huyen'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(cookieParser("huyen"));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
+
+// TinyMCE
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
 
 // Dùng được file tĩnh
 app.use(express.static(`${__dirname}/public`));
