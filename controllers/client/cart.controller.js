a1289e9004ddb01e2448e59ba145bdaec8e8b1bf
NguyenHuyen0311 <huyenmeroria@gmail.com>
2024-07-27 16:33:17 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,41 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/products");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    if(cart.products.length > 0) {
+        for(const item of cart.products) {
+            const productId = item.product_id; // lấy id của từng sản phẩm trong giỏ hàng
+
+            const productInfo = await Product.findOne({
+                _id: productId
+            }); // lấy thông tin sản phẩm
+
+            productInfo.priceNew = productsHelper.priceNewProduct(productInfo); // giá mới
+            
+            item.productInfo = productInfo;
+
+            item.totalPrice = item.quantity * productInfo.priceNew; // tổng tiền sản phẩm
+        } 
+    }
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0); // tổng tiền đơn hàng
+
+    // console.log(cart);
+
+    res.render("client/pages/cart/index", {
+        pageTitle: "Giỏ hàng",
+        cartDetail: cart
+    });
+}
 
 //  [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
