c2e476dc5d09611f7f322824dc985fad389b3398
NguyenHuyen0311 <huyenmeroria@gmail.com>
2024-07-31 19:01:34 +0700

    Tính năng đăng nhập bên client

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -20,3 +20,19 @@ module.exports.registerPost = (req, res, next) => {
 
     next(); // Chạy sang controller
 }
+
+module.exports.loginPost = (req, res, next) => {
+    if(!req.body.email) {
+        req.flash("error", `Email không được để trống!`);
+        res.redirect("back");
+        return;
+    }
+
+    if(!req.body.password) {
+        req.flash("error", `Mật khẩu không được để trống!`);
+        res.redirect("back");
+        return;
+    }
+
+    next(); // Chạy sang controller
+}
